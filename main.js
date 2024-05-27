4ecc2a662d1d67fe0ff1ec886b311a103cb77c67
MeznahAldossari <MeznahAldossari@hotmail.com>
2024-05-27 13:45:26 +0300

    v1

---
+++ main.js
@@ -0,0 +1,201 @@
+function personalImage() {
+  let personalImg = document.getElementById("person");
+  let personalImg2 = document.getElementById("person2");
+  personalImg.style.display = "none";
+  personalImg2.style.display = "block";
+  console.log(personalImg2.style.display);
+}
+
+function imageRemoved() {
+  let personalImg = document.getElementById("person");
+  let personalImg2 = document.getElementById("person2");
+  personalImg.style.display = "block";
+  personalImg2.style.display = "none";
+  console.log(personalImg2.style.display);
+}
+
+// phone
+function MyPhone() {
+  let hisPhone = document.getElementById("phone");
+  let MyPhone1 = document.getElementById("phone2");
+
+  hisPhone.style.display = "none";
+  MyPhone1.style.display = "block";
+}
+
+function hisPhone() {
+  let hisPhone = document.getElementById("phone");
+  let MyPhone1 = document.getElementById("phone2");
+
+  hisPhone.style.display = "block";
+  MyPhone1.style.display = "none";
+}
+
+// email
+function MyEmail() {
+  let hisEmail = document.getElementById("email");
+  let MyEmail1 = document.getElementById("email2");
+
+  hisEmail.style.display = "none";
+  MyEmail1.style.display = "block";
+}
+
+function hisEmail() {
+  let hisEmail = document.getElementById("email");
+  let MyEmail1 = document.getElementById("email2");
+
+  hisEmail.style.display = "block";
+  MyEmail1.style.display = "none";
+}
+
+// Location
+
+function MyLocation() {
+  let hisLocation = document.getElementById("location");
+  let MyLocation = document.getElementById("location2");
+
+  hisLocation.style.display = "none";
+  MyLocation.style.display = "block";
+}
+
+function hisLocation() {
+  let hisLocation = document.getElementById("location");
+  let MyLocation = document.getElementById("location2");
+
+  hisLocation.style.display = "block";
+  MyLocation.style.display = "none";
+}
+
+// Site
+function MySite() {
+  let hisSite = document.getElementById("site");
+  let MySite = document.getElementById("site2");
+
+  hisSite.style.display = "none";
+  MySite.style.display = "block";
+}
+
+function hisSite() {
+  let hisSite = document.getElementById("site");
+  let MySite = document.getElementById("site2");
+
+  hisSite.style.display = "block";
+  MySite.style.display = "none";
+}
+
+//Education
+function MyEducation() {
+  let hisEdu = document.getElementById("education-details");
+  let MyEdu = document.getElementById("Myeducation");
+
+  hisEdu.style.display = "none";
+  MyEdu.style.display = "block";
+}
+
+function hisEducation() {
+  let hisSite = document.getElementById("education-details");
+  let MySite = document.getElementById("Myeducation");
+
+  hisSite.style.display = "block";
+  MySite.style.display = "none";
+}
+
+//Skills
+
+function MySkills() {
+  let hisskill = document.getElementById("hisSkill");
+  let Myskill = document.getElementById("myskill");
+
+  hisskill.style.display = "none";
+  Myskill.style.display = "block";
+}
+
+function hisSkills() {
+  let hisskill = document.getElementById("hisSkill");
+  let Myskill = document.getElementById("myskill");
+
+  hisskill.style.display = "block";
+  Myskill.style.display = "none";
+}
+
+//Languages
+
+function myLanguages() {
+  let hisLanguage = document.getElementById("hisLanguage");
+  let myLanguage = document.getElementById("myLanguage");
+
+  hisLanguage.style.display = "none";
+  myLanguage.style.display = "block";
+}
+
+function hisLanguages() {
+  let hisLanguage = document.getElementById("hisLanguage");
+  let myLanguage = document.getElementById("myLanguage");
+
+  hisLanguage.style.display = "block";
+  myLanguage.style.display = "none";
+}
+
+function myName() {
+  let hisName = document.getElementById("name");
+  let myName = document.getElementById("myname");
+
+  hisName.style.display = "none";
+  myName.style.display = "block";
+}
+
+function hisName() {
+  let hisName = document.getElementById("name");
+  let myName = document.getElementById("myname");
+
+  hisName.style.display = "block";
+  myName.style.display = "none";
+}
+// Profile
+function myProfile() {
+  let hisPro = document.getElementById("disc");
+  let myPro = document.getElementById("description1");
+
+  hisPro.style.display = "none";
+  myPro.style.display = "block";
+}
+
+function hisProfile() {
+  let hisPro = document.getElementById("disc");
+  let myPro = document.getElementById("description1");
+
+  hisPro.style.display = "block";
+  myPro.style.display = "none";
+}
+
+function myExperiance() {
+  let hisExp = document.getElementById("main-experiance");
+  let myExp = document.getElementById("my-experiance");
+
+  hisExp.style.display = "none";
+  myExp.style.display = "block";
+}
+
+function hisExperiance() {
+  let hisExp = document.getElementById("main-experiance");
+  let myExp = document.getElementById("my-experiance");
+
+  hisExp.style.display = "block";
+  myExp.style.display = "none";
+}
+
+function myRef() {
+  let hisRef = document.getElementById("firstRef");
+  let myRef2 = document.getElementById("seccondRef");
+
+  hisRef.style.display = "none";
+  myRef2.style.display = "block";
+}
+
+function hisRef() {
+  let hisRef = document.getElementById("firstRef");
+  let myRef2 = document.getElementById("seccondRef");
+
+  hisRef.style.display = "block";
+  myRef2.style.display = "none";
+}
